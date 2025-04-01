f14d1a8a885f274c51032d0d32b8565fdc2d72c9
Christina Vieau <cvieau@live.com>
2025-04-01 11:13:11 -0400

    added data api calls

---
+++ src/api/toDoData.js
@@ -0,0 +1,84 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getAllData = () =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/toDoItems`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          resolve(Object.values(data));
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
+  });
+
+const getSingleData = (id) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/toDoItems${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const getAllUserData = (id) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/toDoItems${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          resolve(Object.values(data));
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
+  });
+
+const createData = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/toDoItems`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+const updateData = (payload) =>
+  new Promise((resolve, reject) => {
+    fetch(`${endpoint}/toDoItems${payload.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
+
+export { getAllData, createData, getSingleData, getAllUserData, updateData };
